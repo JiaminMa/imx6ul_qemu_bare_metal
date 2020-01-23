61f277599376497d2b9d9b541f3cd4003286c3a3
Jiamin Ma <jiamin.ma_2@nxp.com>
2020-01-23 12:30:20 +0800

    Add 6ul gpio button interrupt poll bit demo.
    Signed-off-by:Jiamin <424337961@qq.com>

--- entry.c
+++ entry.c
@@ -6,11 +6,26 @@
 #include <imx6ul_iomuxc.h>
 #include <imx_gpio.h>
 
+static void test_led(void);
+static void test_button(void);
+
 static void delay()
 {
     uint32_t i, j = 0;
     for (i = 0; i < 65530; i++)
         for(j = 0; j < 2000; j++);
+}
+
+void c_entry()
+{
+    uart_init();
+
+    printf("hello imx6ul bare metal:%x\n");
+
+    while(1) {
+        //test_led();
+        test_button();
+    }
 }
 
 static void test_led()
@@ -32,14 +47,35 @@ static void test_led()
     }
 }
 
-
-void c_entry()
+static void test_button()
 {
-    uart_init();
+    uint32_t button_int_stat = 0;
+    uint8_t led_status = 0;
+    imx_gpio_t *gpio1 = (imx_gpio_t *)0x0209c000UL;
 
-    printf("hello imx6ul bare metal:%x\n");
+    iomuxc_set_mux(IOMUXC_SW_MUX_CTL_PAD_GPIO1_IO03, MUX_MODE_ALT5);
+    gpio_set_output(gpio1, 3);
 
-    while(1) {
-        test_led();
+    iomuxc_set_mux(IOMUXC_SW_MUX_CTL_PAD_UART1_CTS, MUX_MODE_ALT5);
+    gpio_set_input(gpio1, 18);
+    gpio_set_int_cfg(gpio1, 18, RISING_EDGE);
+    gpio_clr_int_stat(gpio1, 18);
+    gpio_unmask_int(gpio1, 18);
+
+    while (1) {
+
+        while((button_int_stat = gpio_get_int_stat(gpio1, 18)) == 0) {
+            delay();
+        }
+
+        printf("button changed\n");
+        led_status = ~led_status;
+        gpio_clr_int_stat(gpio1, 18);
+        if (led_status == 0) {
+            gpio_set_dr(gpio1, 3);
+        } else {
+            gpio_clr_dr(gpio1, 3);
+        }
+        delay();
     }
 }
--- include/imx_gpio.h
+++ include/imx_gpio.h
@@ -7,15 +7,16 @@
 
 #include <stdint.h>
 
-#define     LOW_LEVEL_SENSITIVE     0
-#define     HIGH_LEVEL_SENSITIVE    1
-#define     RISING_EDGE             2
-#define     FALLING_EDGE            3
+#define LOW_LEVEL_SENSITIVE 0
+#define HIGH_LEVEL_SENSITIVE 1
+#define RISING_EDGE 2
+#define FALLING_EDGE 3
 
-typedef struct imx_gpio_tag {
+typedef struct imx_gpio_tag
+{
 
     volatile uint32_t dr;
-    volatile uint32_t gdir; 
+    volatile uint32_t gdir;
     volatile uint32_t psr;
     volatile uint32_t icr1;
     volatile uint32_t icr2;
@@ -55,17 +56,20 @@ static inline void gpio_set_int_cfg(imx_gpio_t *gpio, uint8_t idx, uint8_t cfg)
     uint32_t shift = 0;
     uint32_t mask = 0;
 
-    if (idx < 16) {
+    if (idx < 16)
+    {
         shift = idx * 2;
         mask = 3 << (idx * 2);
         gpio->icr1 &= ~mask;
         gpio->icr1 |= (cfg << shift);
-    } else {
+    }
+    else
+    {
         shift = (idx - 16) * 2;
         mask = 3 << ((idx - 16) * 2);
         gpio->icr2 &= ~mask;
         gpio->icr2 |= (cfg << shift);
-    } 
+    }
 }
 
 static inline void gpio_mask_int(imx_gpio_t *gpio, uint8_t idx)
@@ -78,9 +82,14 @@ static inline void gpio_unmask_int(imx_gpio_t *gpio, uint8_t idx)
     gpio->imr |= (1 << idx);
 }
 
-static inline uint8_t gpio_get_int_stat(imx_gpio_t *gpio, uint8_t idx)
+static inline uint32_t gpio_get_int_stat(imx_gpio_t *gpio, uint8_t idx)
 {
     return gpio->isr & (1 << idx);
+}
+
+static inline void gpio_clr_int_stat(imx_gpio_t *gpio, uint8_t idx)
+{
+    gpio->isr |= (1 << idx);
 }
 
 static inline void gpio_set_edge_sel(imx_gpio_t *gpio, uint8_t idx)
@@ -91,6 +100,16 @@ static inline void gpio_set_edge_sel(imx_gpio_t *gpio, uint8_t idx)
 static inline void gpio_clr_edge_sel(imx_gpio_t *gpio, uint8_t idx)
 {
     gpio->edge_sel &= ~(1 << idx);
+}
+
+static void dump_gpio(imx_gpio_t *gpio)
+{
+    printf("%s isr:%x\n", __func__, gpio->isr);
+    printf("%s icr1:%x\n", __func__, gpio->icr1);
+    printf("%s icr2:%x\n", __func__, gpio->icr2);
+    printf("%s dr:%x\n", __func__, gpio->dr);
+    printf("%s gdir:%x\n", __func__, gpio->gdir);
+    printf("%s imr:%x\n\n", __func__, gpio->imr);
 }
 
 #endif
